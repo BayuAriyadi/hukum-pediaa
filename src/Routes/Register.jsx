5dfb9464752f7e36bdd4cc78a2471e9c2715866e
Muhammad Bayu Ariyadi <bayzuariya@gmail.com>
2024-05-16 18:36:35 +0800

    first commit

---
+++ src/Routes/Register.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+import formRegister from '../Components/formRegister';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <formRegister />
+    </React.StrictMode>
+);
+
+reportWebVitals();
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Banner from './Components/Banner';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <Banner />
+    <Footer />
   </React.StrictMode>
 );
 
