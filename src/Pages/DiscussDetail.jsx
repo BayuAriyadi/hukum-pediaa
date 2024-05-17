a895d1eb0daf2e1ecbf47047370d58b0773f000c
Muhammad Bayu Ariyadi <bayzuariya@gmail.com>
2024-05-17 23:25:09 +0800

    asdfadfsf

--- src/Pages/DiscussDetail.jsx
+++ src/Pages/DiscussDetail.jsx
@@ -1,14 +1,14 @@
 import Footer from "../Components/Footer";
 import ListDiscuss from "../Components/ListDiscuss";
 import Navbar from "../Components/Navbar";
-import Peraturan from "../Components/Peraturan";
+import Peraturan2 from "../Components/Peraturan2";
 import Search from "../Components/Searchbox";
 
 const Diskusi = () => {
     return (
         <>
             <Navbar />
-            <Peraturan />
+            <Peraturan2 />
             <Search />
             <ListDiscuss />
             <Footer />
